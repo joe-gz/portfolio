a14b750a6450282312148ae4cdcdea5070de9f32
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-16 16:05:45 -0400

    social links added

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+//Use the onload event so that we can make sure the DOM is at
+//least mostly loaded before trying to get elements
+window.onload = function() {
+   //Get the DOM element that represents the <button> element.
+   //And set the onclick event
+   document.getElementById("address").onclick = function(){
+      //Set a variable to contain the DOM element of the overly
+      var overlay = document.getElementById("overlay");
+      //Set a variable to contain the DOM element of the popup
+      var popup = document.getElementById("popup");
+
+      //Changing the display css style from none to block will make it visible
+      overlay.style.display = "block";
+      //Same goes for the popup
+      popup.style.display = "block";
+   };
+
+   document.getElementById("popup").addEventListener("click", function(){
+      var popup = document.getElementById("popup");popup.style.display = "none";
+      var overlay = document.getElementById("overlay");
+      overlay.style.display = "none";
+   }, true);
+}
