35655d341b6c71f458292b0dc4383e0785985908
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-02-04 11:11:25 -0500

    adds new images for projects

--- js/projects/project.controller.js
+++ js/projects/project.controller.js
@@ -14,13 +14,13 @@
     {
       title:"beerLogger",
       url:"https://beerlogger.herokuapp.com/",
-      photo_url:"images/beerLogger.png",
+      photo_url:"images/beerLogger2.png",
       description:"A social app, built in Rails and linked to BreweryDB API, allowing users to track and display beers they are drinking."
     },
     {
       title:"trans.HIT",
       url:"https://trans-hit.herokuapp.com/",
-      photo_url:"images/transHIT.png",
+      photo_url:"images/transHIT2.png",
       description:"An interactive OOJS-built app that provides a means through which DC metro riders can discuss issues occuring in specific stations"
     }
   ]});
